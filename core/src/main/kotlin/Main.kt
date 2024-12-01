75603ac82e689dc1fb6306fc571a5c70b1f9a6b7
rodkot <rodtdt@yandex.ru>
2024-12-01 22:49:31 +0700

    Добавил первый вариант документации

--- core/src/main/kotlin/Main.kt
+++ core/src/main/kotlin/Main.kt
@@ -1,82 +1,101 @@
 package ru.nsu.fit.mmp.pipelinesframework
 
+import java.util.concurrent.ConcurrentLinkedQueue
+
 fun main() {
     println("Hello World!")
 }
 
-/*
-class Pipe<T>
-class Node(name: String);
+
+class Pipe<T> {
+    private val queue = ConcurrentLinkedQueue<T>()
+
+    inner class Consumer {
+        fun onListener(action: (T) -> Unit) {
+
+        }
+    }
+
+    inner class Producer {
+        fun commit(value: T) {
+
+        }
+    }
+}
+
+class Node(
+    name: String,
+//           inputPipes: List<*>,
+//           outputPipes: List<*>
+)
+
 class Workflow(val nodes: List<Node>)
 
-class Producer<T>(){
-		fun onListener(action: ()->Unit){
-
-		}
-}
-class Consumer<T>(){
-		fun commit(value: T){
-
-		}
-}
-
 class WorkflowBuilder {
-		private val nodes = mutableListOf<Node>()
+    private val nodes = mutableListOf<Node>()
 
-		fun <T, Q> node(name: String, input: Pipe<T>, output: Pipe<Q>) {
-				nodes.add(Node(name))
-		}
+    fun <T, Q> node(
+        name: String,
+        input: Pipe<T>,
+        output: Pipe<Q>,
+        action: (Pipe<T>.Consumer, Pipe<Q>.Consumer) -> Unit
+    ) {
+    }
 
-		fun <T, Q, S> node(name: String, input: Pair<Pipe<T>, Pipe<Q>>, output: Pipe<S>) {
-				nodes.add(Node(name))
-		}
+    fun <T, Q, S, P> node(
+        name: String,
+        input: Pair<Pipe<T>, Pipe<Q>>,
+        output: Pair<Pipe<S>, Pipe<P>>,
+        action: (Pipe<T>.Consumer, Pipe<Q>.Consumer, Pipe<S>.Producer, Pipe<P>.Producer) -> Unit
+    ) {
+    }
 
-		fun <T, Q, S, P> node(
-			name: String,
-			input: Pair<Pipe<T>, Pipe<Q>>,
-			output: Pair<Pipe<S>, Pipe<P>>,
-			action: (Producer<T>, Producer<Q>, Consumer<S>, Consumer<P>) -> Unit
-		) {
-				nodes.add(Node(name))
-		}
+    fun sharedWorkflow(workflow: () -> Workflow) {
+    }
 
-		fun build(): Workflow {
-				return Workflow(nodes)
-		}
+    fun build(): Workflow {
+        return Workflow(nodes)
+    }
 }
 
 fun Workflow(init: WorkflowBuilder.() -> Unit): Workflow {
-		return WorkflowBuilder().apply(init).build()
-}
-
-
- inline fun <T,Q,W> sharedWorkflow (input: Pipe<T>, output: Pipe<Q>, firstParam : W): Workflow {}
-
-
-
-val sharedFlow = sharedWorkflow(firstParam:Int, secondParam){
-		input: Pair<Int, Int>, output: Pair<Int, Int> ->
-
-
+    return WorkflowBuilder().apply(init).build()
 }
 
 val workflow = Workflow {
 
-		val ip = Pipe<Int>()
-		val an = Pipe<Float>()
-		val gk = Pipe<Double>()
+    val ip = Pipe<Int>()
+    val an = Pipe<Float>()
+    val gk = Pipe<Double>()
 
-		node(
-				name = "Первая нода",
-				input = Pair(ip, an),
-				output = Pair(ip, gk)
-		) { a: Producer<Int>, b: Producer<Float>, c: Consumer<Int>, d: Consumer<Double> ->
-				// Define the action logic here
-			(a+b).onListener {
-				Pair()
-				}
-				println("Executing action for node 'Первая нода'")
-				// e.g., Use producers and consumers as needed
-		}
+    sharedWorkflow {
+        mySharedFlow(input = ip, output = ip, 11)
+    }
+
+    node(
+        name = "Первая нода",
+        input = Pair(ip, an),
+        output = Pair(ip, gk)
+    ) { a, b, c, d ->
+        // Define the action logic here
+
+        println("Executing action for node 'Первая нода'")
+        // e.g., Use producers and consumers as needed
+    }
 }
-*/
+
+
+fun mySharedFlow(input: Pipe<Int>, output: Pipe<Int>, firstParam: Int): Workflow =
+    Workflow {
+
+        node(
+            name = "Первая нода",
+            input = input,
+            output = output
+        ) { a, b ->
+            // Define the action logic here
+            println("Executing action for node 'Первая нода'")
+            // e.g., Use producers and consumers as needed
+        }
+    }
+
