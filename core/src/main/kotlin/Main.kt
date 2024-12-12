9bbe199c3c1b9f211b02fddaf0b0d48b499e4a5b
rodkot <rodtdt@yandex.ru>
2024-12-12 12:08:51 +0700

    Добавил для экспериментов Егора с ошибкой создания nodes без input

--- core/src/main/kotlin/Main.kt
+++ core/src/main/kotlin/Main.kt
@@ -23,39 +23,43 @@ fun main() {
             sequenceGenerator(randomPipe1)
         }
 
-        sharedWorkflow {
-            sequenceGenerator(randomPipe2)
-        }
+//        sharedWorkflow {
+//            sequenceGenerator(randomPipe2)
+//        }
 
-        node(
-            "Лишняя нода",
-            input = Pair(randomPipe1, randomPipe2),
-            output = Pair(outputPipe1, outputPipe2)
-        ) { consumer, consumer2, producer, producer2 ->
-            (consumer + consumer2).onListener { value1, value2 ->
-                (producer + producer2).commit(value1.toDouble(), value2.toDouble())
-            }
-
-            println("hello")
-        }
-
-        finish(
-            "Лишняя нода 2",
-            input = outputPipe1,
-        ) { consumer ->
-            consumer.onListener{
-                value ->
-                println("Лишняя нода 2 $value")
-            }
-        }
+//        node(
+//            "Лишняя нода",
+//            input = Pair(randomPipe1, randomPipe2),
+//            output = Pair(outputPipe1, outputPipe2)
+//        ) { consumer, consumer2, producer, producer2 ->
+//            (consumer + consumer2).onListener { value1, value2 ->
+//                (producer + producer2).commit(value1.toDouble(), value2.toDouble())
+//            }
+//
+//            println("hello")
+//        }
+//
+//        finish(
+//            "Лишняя нода 2",
+//            input = outputPipe1,
+//        ) { consumer ->
+//            consumer.onListener{
+//                value ->
+//                println("Лишняя нода 2 $value")
+//            }
+//        }
 
 
         finish(
             name = "Print", input = randomPipe1
         ) { consumer ->
-            consumer.onListener { value ->
-                println(value)
+            (consumer+consumer).onListener { value1, value2 ->
+                println("$value1 - $value2")
             }
+
+//            consumer.onListener { value ->
+//                println(value)
+//            }
         }
     }
 
--- core/src/main/kotlin/Pipe.kt
+++ core/src/main/kotlin/Pipe.kt
@@ -1,8 +1,6 @@
 package ru.nsu.fit.mmp.pipelinesframework
 
 import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.coroutineScope
-import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.SharedFlow
 import kotlinx.coroutines.flow.combine
@@ -37,7 +35,7 @@ sealed class Pipe {
             }
 
             operator fun <U> plus(other: Single<U>.Producer): Dual<T, U>.Producer {
-                return Dual(_flow, other.flow).Producer(coroutineScope)
+                return Dual(_flow, other.flow).Producer()
             }
         }
     }
@@ -49,22 +47,20 @@ sealed class Pipe {
         val flowT: SharedFlow<T> get() = _flowT
         val flowU: SharedFlow<U> get() = _flowU
 
-        inner class Consumer(private val coroutineScope: CoroutineScope)  {
+        inner class Consumer(private val coroutineScope: CoroutineScope) {
             fun onListener(action: (T, U) -> Unit) {
                 coroutineScope.launch {
                     flowT.combine(flowU) { valueT, valueU ->
                         valueT to valueU
-                    }.collect{ (t, u) -> action(t, u)}
+                    }.collect { (t, u) -> action(t, u) }
                 }
             }
         }
 
-        inner class Producer(private val coroutineScope: CoroutineScope)  {
-            fun commit(value1: T, value2: U) {
-                coroutineScope.launch {
-                    _flowT.emit(value1)
-                    _flowU.emit(value2)
-                }
+        inner class Producer {
+            suspend fun commit(value1: T, value2: U) {
+                _flowT.emit(value1)
+                _flowU.emit(value2)
             }
         }
     }
