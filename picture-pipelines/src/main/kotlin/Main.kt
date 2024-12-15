f5d0a1ed8dbc43fabc70441c7f28eeb3b41cc7ff
egorkuzn2002 <georkuzn2002@mail.ru>
2024-12-15 21:10:12 +0700

    реализация-на-channel: ну что за... хотел практику делать. Короче, тут на каналах реализация.

--- picture-pipelines/src/main/kotlin/Main.kt
+++ picture-pipelines/src/main/kotlin/Main.kt
@@ -1,13 +1,8 @@
 package ru.nsu.fit.mmp.pipelinesframework
 
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.*
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.channels.produce
-import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.flow.receiveAsFlow
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
 
 fun line() = println("-".repeat(30))
 
@@ -15,45 +10,100 @@ fun line() = println("-".repeat(30))
 fun main() {
     println("Pipes")
     line()
-    runBlocking {
-        Workflow(dispatcher = Dispatchers.Default.limitedParallelism(4)) {
-            val symbols = Pipe<String>()
-
-            node(
-                name = "Выведи символ 'a' n раз",
-                input = Init(1, 2, 3, 4, 5),
-                output = symbols
-            ) { consumer, producer ->
-                consumer.onListener { launch { producer.commit("a".repeat(it)) } }
-            }
-
-            node(
-                name = "Принтер с улыбкой",
-                input = symbols,
-                output = Finish
-            ) { consumer, _ ->
-                consumer.onListener { println("$it)") }
-            }
-
-            node(
-                name = "Принтер",
-                input = symbols,
-                output = Finish
-            ) { consumer, _ ->
-                consumer.onListener { println(it) }
-            }
-        }.start()
-    }
+//    runBlocking {
+//        Workflow(dispatcher = Dispatchers.Default.limitedParallelism(4)) {
+//            val symbols = Pipe<String>()
+//
+//            node(
+//                name = "Выведи символ 'a' n раз",
+//                input = Init(1, 2, 3, 4, 5),
+//                output = symbols
+//            ) { consumer, producer ->
+//                consumer.onListener { launch { producer.commit("a".repeat(it)) } }
+//            }
+//
+//            node(
+//                name = "Принтер с улыбкой",
+//                input = symbols,
+//                output = Finish
+//            ) { consumer, _ ->
+//                consumer.onListener { println("$it)") }
+//            }
+//
+//            node(
+//                name = "Принтер",
+//                input = symbols,
+//                output = Finish
+//            ) { consumer, _ ->
+//                consumer.onListener { println(it) }
+//            }
+//        }.start()
+//    }
     line()
     println("Channels")
     line()
     runBlocking {
-        Workflow(dispatcher = Dispatchers.Default.limitedParallelism(4)) {
-            val numbers = produce { (1..5).map { send(it) } }
-            val symbols = produce { for (msg in numbers)  send("a".repeat(msg)) }
-            launch { for (msg in symbols) println("${msg})")  }
-            launch { for (msg in symbols) println(msg)  }
-        }.start()
+        val numbers = produce { (1..10).map { send(it) } }
+        val symbols = produce {
+            for (msg in numbers) launch {
+                send("a".repeat(msg))
+            }
+        }
+        launch {
+            for (msg in symbols) launch {
+                println("${msg})")
+            }
+        }
+        launch {
+            for (msg in symbols) launch {
+                println(msg)
+            }
+        }
+        delay(1000)
+        numbers.cancel()
+        symbols.cancel()
     }
+    line()
+    println("nnode")
+    line()
+    runBlocking {
+        launch {
+            Wworkflow(Dispatchers.Default.limitedParallelism(4)) {
+                val numbers = pproduce { (1..10).map { send(it) } }
+                val symbols = cchanel<String>()
+
+                nnode(
+                    name = "Выведи символ 'a' n раз",
+                    input = numbers,
+                    output = symbols
+                ) {
+                    "a".repeat(it)
+                }
+
+                nnode(
+                    name = "Принтер с улыбкой",
+                    input = symbols
+                ) {
+                    println("$it)")
+                }
+
+                nnode(
+                    name = "Принтер",
+                    input = symbols
+                ) {
+                    println(it)
+                }
+
+                println("Yeah")
+            }
+        }
+
+        launch {
+            delay(1000)
+            stop()
+        }
+    }
+
+
     line()
 }
